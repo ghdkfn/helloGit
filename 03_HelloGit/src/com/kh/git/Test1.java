fbeb9d16b44691c6908de577c05d3497f6913de1
ghdkfn <ghdkfn@naver.com>
2018-06-12 13:16:41 +0900

    HY
    두번째 저장

--- 03_HelloGit/src/com/kh/git/Test1.java
+++ 03_HelloGit/src/com/kh/git/Test1.java
@@ -3,7 +3,8 @@ package com.kh.git;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("안녕,깃");
+		System.out.println("Hello, git");
 
 	}
 
